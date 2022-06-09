adff4f77d49ad7ce1cad1a32d08edd3ad6fd0dcb
Attmoon <99157555+Attmoon@users.noreply.github.com>
2022-06-09 17:47:41 +0900

    createt files(controller, dto, service)

---
+++ src/main/java/com/hys/trazar/controller/RequestController.java
@@ -0,0 +1,10 @@
+package com.hys.trazar.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("request")
+public class RequestController {
+	
+}
---
+++ src/main/java/com/hys/trazar/domain/RequestDto.java
@@ -0,0 +1,8 @@
+package com.hys.trazar.domain;
+
+import lombok.Data;
+
+@Data
+public class RequestDto {
+
+}
