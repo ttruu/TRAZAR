b63a19abb945d12c3c7ccff5582b5bb4f0be58a7
Yongssun <99157768+Yongssun@users.noreply.github.com>
2022-06-16 16:55:23 +0900

    디자이너 보드 보이기

--- src/main/java/com/hys/trazar/controller/DesignerInfo.java
+++ src/main/java/com/hys/trazar/controller/DesignerInfo.java
@@ -1,34 +1,34 @@
 package com.hys.trazar.controller;
 
-import java.security.Principal;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.hys.trazar.domain.RequestDto;
 import com.hys.trazar.domain.login.SignupDto;
 import com.hys.trazar.mapper.login.SignupMapper;
 import com.hys.trazar.service.login.SignupService;
 
-@Controller("designerInfo")
+@Controller
+@RequestMapping("designer")
 public class DesignerInfo {
 	@Autowired
 	private SignupService service;
 
 	@Autowired
 	private SignupMapper mapper;
+
 	
-	@RequestMapping(value = "/view", method = RequestMethod.GET)
-	public void Infoget(String id, Model model) throws Exception {
+	@GetMapping("view")
+	public String getInfo(String id, Model model) {
+		/* 1.DB에서 값가져오기 */
 		SignupDto dto = service.selectMember(id);
-		model.addAttribute("model", id);
+		/* 2.jsp 지정한 변수로 값 넣기 */
+	model.addAttribute("member", dto);
 	
-	}
+	/* 여기로 보내라 */
+	return "/designer/view";
+}
 	
 }
